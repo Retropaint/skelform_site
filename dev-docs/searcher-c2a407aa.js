840b646167c97dbfa39338d6393abbe7e66be6ed
Retropaint <darkglasses1122@gmail.com>
2026-03-09 04:39:42 +0700

    updated dev docs

--- dev-docs/searcher-c2a407aa.js
+++ dev-docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bb2c3939.js',
+                path_to_root + 'searchindex-f2c91639.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
