0968f438459bb22455cec401448173b28277d744
Retropaint <darkglasses1122@gmail.com>
2026-03-20 05:33:21 +0700

    updated dev-docs

--- dev-docs/searcher-c2a407aa.js
+++ dev-docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-57eb42ab.js',
+                path_to_root + 'searchindex-5848574c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
