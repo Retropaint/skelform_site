4ac089baa1c37fc051d5afc220116ea5d8eca655
Retropaint <darkglasses1122@gmail.com>
2026-06-18 16:29:58 +0700

    updated dev-docs

--- dev-docs/searcher-09f2665d.js
+++ dev-docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d7f99ae3.js',
+                path_to_root + 'searchindex-20257651.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
